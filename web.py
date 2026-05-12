4f0cc89148257dc3cc07c0a0ce920ca5c05af50a
linxiao0519-eng <linxiao0519@gmail.com>
2026-05-12 11:42:09 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -50,14 +50,13 @@ def webhook():
     # build a request object
     req = request.get_json(force=True)
     # fetch queryResult from json
-    action =  req["queryResult"]["action"]
-    #msg =  req["queryResult"]["queryText"]
-    #info = "我是陳楷修設計的電影聊天機器人，動作：" + action + "； 查詢內容：" + msg
+    action =  req.get("queryResult").get("action")
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
     if (action == "rateChoice"):
-        rate =  req["queryResult"]["parameters"]["rate"]
-        info = "我是陳楷修設計的電影聊天機器人，您選擇的電影分級是：" + rate
+        rate =  req.get("queryResult").get("parameters").get("rate")
+        info = "我是陳楷修開發的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
 
-    return make_response(jsonify({"fulfillmentText": info}))
 
 
 @app.route("/weather", methods=["GET", "POST"])
