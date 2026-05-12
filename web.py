f9ef6d9a0867e871648764bcb602df803e0c9d4a
linxiao0519-eng <linxiao0519@gmail.com>
2026-05-12 11:49:18 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -58,7 +58,7 @@ def webhook():
         info = "我是陳楷修開發的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
 
         db = firestore.client()
-        collection_ref = db.collection("電影含分級")
+        collection_ref = db.collection("本週新片含分級")
         docs = collection_ref.get()
         result = ""
         for doc in docs:
