5a159957bd73fece0102c95b3d07660ce0c83fa8
linxiao0519-eng <linxiao0519@gmail.com>
2026-05-26 11:42:22 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -113,7 +113,8 @@ def webhook():
         info += result
     elif (action == "input.unknown"):
             ai_config = types.GenerateContentConfig(
-        max_output_tokens = 5000
+        max_output_tokens = 3000
+    
     )
     response = client.models.generate_content(
         model='gemini-3.5-flash', 
